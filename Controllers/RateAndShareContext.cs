cb91c02c8ace446efadf45f21d309e7a321268b1
assaf <assafzvigoren4@gmail.com>
2019-08-10 17:21:18 +0300

    start working on connection with EF

--- Controllers/RateAndShareContext.cs
+++ Controllers/RateAndShareContext.cs
@@ -5,11 +5,19 @@ namespace RateAndShare.Controllers
 {
     public class RateAndShareContext : DbContext
     {
-        public RateAndShareContext()
+        public RateAndShareContext() : base("name=RateAndShareContext")
         {
 
         }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>()
+                .HasKey(song => song.SongId);
+
+            modelBuilder.Entity<Rate>()
+                .HasKey(rate => rate.ID);
+        }
         public DbSet<Song> Songs { get; set; }
         public DbSet<Rate> Rates { get; set; }
     }
