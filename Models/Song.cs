3b3172b5c011f8a2fdaf3bdb24a0de6e16155c5e
ghershko3 <ghershko3@gmail.com>
2019-08-10 11:25:35 +0300

    add song model

---
+++ Models/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RateAndShare.Models
+{
+    public class Song
+    {
+        public int SongId { get; set; }
+        public string SongName { get; set; }
+        public string ArtistName { get; set; }
+        public string Genre { get; set; }
+        public string YoutubeLink { get; set; }
+    }
+}
