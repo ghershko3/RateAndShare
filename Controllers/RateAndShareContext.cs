0d8ca4c62616b9cb1d65f0b71bcfebf063ec639f
assaf <assafzvigoren4@gmail.com>
2019-08-10 16:49:40 +0300

    fix the error compilations caused by the db context

--- Controllers/RateAndShareContext.cs
+++ Controllers/RateAndShareContext.cs
@@ -1,9 +1,16 @@
-﻿using System.Data.Entity;
+﻿using RateAndShare.Models;
+using System.Data.Entity;
 
 namespace RateAndShare.Controllers
 {
     public class RateAndShareContext : DbContext
     {
+        public RateAndShareContext()
+        {
 
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Rate> Rates { get; set; }
     }
 }
